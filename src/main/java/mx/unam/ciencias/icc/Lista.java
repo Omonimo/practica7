96813762746431d2f552f811df64f2cd58d0060c
omonimo <alfredom.garciag@gmail.com>
2025-10-28 04:37:13 -0600

    codigo compila y pasa pruebas

--- src/main/java/mx/unam/ciencias/icc/Lista.java
+++ src/main/java/mx/unam/ciencias/icc/Lista.java
@@ -134,7 +134,6 @@ public class Lista<T> implements Iterable<T> {
         if(elemento == null)
             throw new IllegalArgumentException();
         Nodo n = new Nodo(elemento);
-        longitud++;
         if(esVacia()){
             cabeza = rabo = n;
         } else {
@@ -142,6 +141,7 @@ public class Lista<T> implements Iterable<T> {
             n.anterior = rabo;
             rabo = n;
         }
+        longitud++;
     }
 
     /**
@@ -156,7 +156,6 @@ public class Lista<T> implements Iterable<T> {
         if(elemento == null)
             throw new IllegalArgumentException();
         Nodo n = new Nodo(elemento);
-        longitud++;
         if(esVacia()){
             cabeza = rabo = n;
         } else{
@@ -164,6 +163,7 @@ public class Lista<T> implements Iterable<T> {
             n.siguiente = cabeza;
             cabeza = n;
         }
+        longitud++;
     }
 
     /**
